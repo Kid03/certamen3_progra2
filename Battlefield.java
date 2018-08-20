e393e9314529bfc61f9b171d063c484d7f9a4606
Kid03 <34219487+Kid03@users.noreply.github.com>
2018-08-20 19:52:03 -0300

    Creacion de Battlefield
    Función que maneja a los personajes y loe lleva a combate

---
+++ Battlefield.java
@@ -0,0 +1,12 @@
+public class Battlefield {
+        private Guerrero warrior;
+        private Mago wizard;
+        private String direccion="";
+        public Battlefield (Guerrero warrior,Mago wizard){
+                this.warrior=warrior;
+                this.wizard=wizard;
+        }
+        public void Combatir (){
+
+        }
+}
