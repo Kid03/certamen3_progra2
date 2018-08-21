b7b088209e5e67f95df8cfc5286a4208026cb0f2
Kid03 <34219487+Kid03@users.noreply.github.com>
2018-08-21 20:19:43 -0300

    Continuacion de Battlefield
    Pequeño avance en la implementación para los eventos de combate

--- Battlefield.java
+++ Battlefield.java
@@ -1,12 +1,29 @@
 public class Battlefield {
-        private Guerrero warrior;
-        private Mago wizard;
-        private String direccion="";
-        public Battlefield (Guerrero warrior,Mago wizard){
-                this.warrior=warrior;
-                this.wizard=wizard;
+        private Personaje p1;
+        private Personaje p2;
+        private PrintWriter p;
+        private String atkP1;
+        private String atkP2;
+        public Battlefield (Personaje p1,Personaje p2, PrintWriter p){
+                this.p1=p1;
+                this.p2=p2;
+                this.p=p;
         }
         public void Combatir (){
-
+                Integer STP1=p1.energia;
+                Integer STP2=p2.energia;
+                if (p1 instanceof Guerrero){
+                        atkP1=p1.combatir();
+                }else{
+                        atkP1=p1.encantar();
+                }
+                if (p2 instanceof Guerrero){
+                        atkP2=p2.combatir();
+                }else {
+                        atkP2=p2.encantar();
+                }
+                atkP1.split(";");
+                atkP2.split(";");
+                
         }
 }
